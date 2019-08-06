b3d956689884f15e075aaa54b29294dcc9933b29
Eric Miao <hongyum@smu.edu>
2019-08-06 12:42:01 -0500

    Import_CSV works

--- ImportCSV.js
+++ ImportCSV.js
@@ -1,59 +1,27 @@
 /*
     Import CSV files and convert to AdjList data structure
-    Import using Angular
+    Import using fs
 */
+
 console.log("This is Import CSV function");
-let attendeesArray = [];
-let fileUploaded = false;
-function handleFiles(files)
-{
-    if(window.fileReader)
-    {
-        getAsText(files[0]);
-        fileUploaded = true;
-    } else 
-    {
-        alert ("File reader not supported");
-    }
+var fs = require('fs');
+
+try {
+    var data = fs.readFileSync('test_data_small.csv', 'utf8');
+
+} catch (e) {
+    console.log('ERROR in ImportCSV Module: Cannot find File', e.stack);
 }
 
-function getAsText(fileToRead)
-{
-    let reader = new FileReader();
-
-    reader.onload = loadHandler;
-    reader.onerror = errorHandler;
-}
-
-function loadHandler (event)
-{
-    let csv = event.target.result;
-    processData(csv);
-}
-
-function errorHandler(event)
-{
-    if(event.target.error.name == "NotReadableError")
-    {
-        alert('ERROR: Cannot read file!');
-    }
-}
-
-//NOTE: Convert to Array for now and will add to AdjList once implemented 
-function processData(csv)
-{
-    let allTextLines = csv.split(/\r\n|\n);
-    for (let i = 0; i < allTextLines.length; i++)
-    {
-        let row = allTextLines[i].split(';');
-        let col = []; //empty array for now
-        for( j = 0; j < row.length; j++)
-        {
-            col.push(row[j]);
-        }
-        attendeesArray.push(col);
-    }
-
+let res = data.split('\n');
+console.log("First row of the data: ", res[1]);
+for (i in res) {
+    let element = res[i];
+    let indv = element.split(',');
+    console.log(indv);
+    console.log(" ");
 }
 
 
+//var news = res.split('\r');å
+// console.log(res);  
